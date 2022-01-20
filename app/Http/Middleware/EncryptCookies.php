7db00e76fe8e2eb5a14b0a582c4a086cc240877c
Duman Haydar <haydar@duman.io>
2022-01-20 21:07:51 +0100

    init laravel auth keycloak

---
+++ app/Http/Middleware/EncryptCookies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
+
+class EncryptCookies extends Middleware
+{
+    protected $except = [];
+
+    public function __construct(EncrypterContract $encrypter)
+    {
+        parent::__construct($encrypter);
+
+        /**
+         * This will disable in runtime.
+         *
+         * If you have a "session.cookie" option or don't care about changing the app name
+         * (in another environment, for example), you can only add it to "$except" array on top
+         */
+        $this->disableFor(config('session.cookie'));
+    }
+}
